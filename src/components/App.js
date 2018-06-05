9c680e99f680a0c2fa73bb5ee5d6afe07d7653e6
hammadyounas <m.hammadyounas@outlook.com>
2018-06-05 15:18:29 +0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './Hello'
+
+class App extends React.Component {
+    render() {
+      return (
+        // <div className="shopping-list">
+        <Hello/>
+        //   {/* <h1>Shopping List for {this.props.name}</h1>
+        //   <ul>
+        //     <li>Instagram</li>
+        //     <li>WhatsApp</li>
+        //     <li>Oculus</li>
+        //   </ul>
+        // </div> */}
+      );
+    }
+  }
+  export default App;
