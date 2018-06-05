aabe5a003822ac426af36596abba7da3eb800636
hammadyounas <m.hammadyounas@outlook.com>
2018-06-05 18:06:34 +0500

    working

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -3,29 +3,54 @@ import ReactDOM from 'react-dom';
 import './todo.css'
 
 class Todo extends React.Component {
-    constructor(){
+    constructor(props) {
         super()
+        this.state = {
+            value:'hammad',
+            id:25,
+            list:['hammad     ', 'mohsin ', 'taimoor ']
+        }
+        this.add = this.add.bind(this)
+        this.onValueChange = this.onValueChange.bind(this)
     }
-    update(){
+    update() {
 
     }
-    delete(){
+    delete() {
 
     }
-    add(){
-        
+    add() {
+        // alert("asujhdysja");
+        console.log("this.state.value",this.state.value)
+    }
+    onValueChange(e){
+        console.log(e.target.value);
+        this.setState({
+            value:e.target.value
+        })
     }
     render() {
+
         return (
             <div className="card cardStyle">
-            <h5 className="card-header">TODO APP</h5>
+                <h5 className="card-header">TODO APP {this.state.name}</h5>
                 <div className="card-body">
                     <form>
                         <div className="input-group">
-                            <input type="text" className="form-control" placeholder="Value"/>
-                            <button className="btn btn-default" type="submit">Add</button>
+                            <input type="text" className="form-control" value={this.state.value} onChange={this.onValueChange} placeholder="Value" />
+                            <button className="btn btn-default" type="submit" onClick={this.add}>Add</button>
                         </div>
                     </form>
+                    <div>
+                        <ul>
+                        {
+                            this.state.list.map((item,i)=>{
+                                return ( <li key={i}>{item}</li> )
+                            })
+                        }
+                        </ul>
+
+                    </div>
                 </div>
             </div>
         )
